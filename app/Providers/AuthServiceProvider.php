2e3a39cb0c8669633bb6cbaed67b473e5d4f57be
syauqidamario <syauqi.djohan@student.umn.ac.id>
2024-01-20 16:34:58 +0700

    Gate Facade.

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,13 +3,19 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
-
+use App\Models\Contact;
+use App\Models\Todo;
+use App\Models\User;
+use App\Policies\TodoPolicy;
+use App\Policies\UserPolicy;
 use App\Providers\Guard\TokenGuard;
 use App\Providers\User\SimpleUserProvider;
+use Illuminate\Auth\Access\Response;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -19,7 +25,8 @@ class AuthServiceProvider extends ServiceProvider
      * @var array<class-string, class-string>
      */
     protected $policies = [
-        //
+        // Todo::class => TodoPolicy::class,
+        User::class => UserPolicy::class
     ];
 
     /**
@@ -28,13 +35,30 @@ class AuthServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Auth::extend("token", function (Application $app, string $name, array $config) {
-            $guard = new TokenGuard(Auth::createUserProvider($config["provider"]), $app->make(Request::class));
-            $app->refresh('request', $guard, 'setRequest');
-            return $guard;
+            $tokenGuard = new TokenGuard(Auth::createUserProvider($config["provider"]), $app->make(Request::class));
+            $app->refresh("request", $tokenGuard, "setRequest");
+            return $tokenGuard;
         });
 
         Auth::provider("simple", function (Application $app, array $config) {
             return new SimpleUserProvider();
+        });
+
+        Gate::define("get-contact", function (User $user, Contact $contact) {
+            return $user->id == $contact->user_id;
+        });
+        Gate::define("update-contact", function (User $user, Contact $contact) {
+            return $user->id == $contact->user_id;
+        });
+        Gate::define("delete-contact", function (User $user, Contact $contact) {
+            return $user->id == $contact->user_id;
+        });
+        Gate::define("create-contact", function (User $user) {
+            if ($user->name == "admin") {
+                return Response::allow();
+            } else {
+                return Response::deny("You are not admin");
+            }
         });
     }
 }
